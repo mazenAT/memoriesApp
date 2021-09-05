02efd08a257b8f3b9d8e4f8481a34a5900e21a89
mazenAT <42847172+mazenAT@users.noreply.github.com>
2021-09-05 23:35:50 +0200

    Add files via upload

---
+++ src/reducers/Posts.js
@@ -0,0 +1,17 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+export default (Posts=[],action)=>{
+    switch (action.type) {
+        case FETCH_ALL:
+            return action.payload;
+        case CREATE:
+            return [...Posts,action.payload];
+        case UPDATE:
+            return Posts.map((post) => post._id===action.payload._id? action.payload : post)
+        case DELETE:
+            return Posts.filter((post)=> post._id !==action.payload);
+        case LIKE:
+            return Posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+        default:
+            return Posts;
+    }
+}
